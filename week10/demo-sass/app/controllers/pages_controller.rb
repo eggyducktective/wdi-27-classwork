cc2641ac0f9b140f78fb96264dbca150a961b0a7
Anna Tran <anhkaehne@gmail.com>
2018-06-28 11:49:19 +1000

    flex, grid and media query

--- week10/demo-sass/app/controllers/pages_controller.rb
+++ week10/demo-sass/app/controllers/pages_controller.rb
@@ -1,4 +1,15 @@
 class PagesController < ApplicationController
   def home
   end
+
+
+  def flex
+  end
+
+  def grid
+  end
+
+
+  def responsive
+  end 
 end
--- week10/demo-sass/config/routes.rb
+++ week10/demo-sass/config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
   root to: 'pages#home'
+
+  get '/flex'       => 'pages#flex'
+  get '/grid'       => 'pages#grid'
+  get '/responsive' => 'pages#responsive'
 end
